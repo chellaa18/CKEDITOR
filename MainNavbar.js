263a73033a81892e3b9e2e2c98414ce61a651841
Chellaa <98450157+chellaa18@users.noreply.github.com>
2024-01-12 22:59:15 +0530

    Update MainNavbar.js

--- MainNavbar.js
+++ MainNavbar.js
@@ -2,48 +2,30 @@
 import React from "react";
 import { Link } from "react-router-dom";
 
-const MainNavbar = ({ activeContent }) => {
-  const getTitle = () => {
-    switch (activeContent) {
-      case "dashboard":
-        return "Dashboard";
-      case "User Detail":
-        return "User Detail";
-      case "products":
-        return "Products";
-      case "ContentTable":
-        return "Content Table";
-      default:
-        return "Admin Panel";
-    }
-  };
-
-  return (
-    <nav className="navbar navbar-expand-lg navbar-dark border">
-      <div className="container-fluid">
-        <button
-          className="navbar-toggler"
-          type="button"
-          data-bs-toggle="collapse"
-          data-bs-target="#navbarNav"
-          aria-controls="navbarNav"
-          aria-expanded="false"
-          aria-label="Toggle navigation"
-        >
-          <span className="navbar-toggler-icon"></span>
-        </button>
-        <div className="collapse navbar-collapse" id="navbarNav">
-          <ul className="navbar-nav">
-            <li className="nav-item">
-              <Link className="nav-link text-dark fs-5" >
-                {getTitle()}
-              </Link>
-            </li>
-          </ul>
+const MainNavbar = ({ activeContent, onToggleSidebar, isSidebarOpen }) => {
+    return (
+      <nav className="navbar navbar-expand-lg navbar-dark border">
+        <div className="container-fluid">
+          <button
+            className="navbar-toggler"
+            type="button"
+            data-bs-toggle="collapse"
+            data-bs-target="#navbarNav"
+            aria-controls="navbarNav"
+            aria-expanded="false"
+            aria-label="Toggle navigation"
+          >
+            <span className="navbar-toggler-icon"></span>
+          </button>
+          <div className="collapse navbar-collapse d-flex" id="navbarNav">
+          <Link className="me-3" onClick={onToggleSidebar}>
+          {isSidebarOpen ? "Close" : "Open"}
+        </Link>
+            <h4>Navbar</h4>
+          </div>
         </div>
-      </div>
-    </nav>
-  );
-};
+      </nav>
+    );
+  };
 
 export default MainNavbar;
